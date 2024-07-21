da27b115ab2d2357e401128840e67125e8de50cc
nanditha121 <nanditha460@gmail.com>
2024-07-21 22:01:32 +0530

    first commit

---
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+export default function HomePage(){
+    return(
+        <>
+        <div className="homepage">
+        <h1>Welcome to Our College</h1>
+        <img src="./sec.jpg"></img>
+            </div>
+         
+   
+    </>
+    )
+}
